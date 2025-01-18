527c4022aeab57cfbb85319d91b1d51ce0d9f98d
Abdul Rehman <ar618211@gmail.com>
2025-01-18 20:03:16 +0500

    loads of animation stuff: hit, squash, weapon switch

--- scenes/entities/player/godette_skin.gd
+++ scenes/entities/player/godette_skin.gd
@@ -1,17 +1,53 @@
 extends Node3D
 
 @onready var move_state_machine = $AnimationTree.get("parameters/MoveStateMachine/playback")
+@onready var attack_state_machine = $AnimationTree.get("parameters/AttackStateMachine/playback")
+@onready var extra_animation = $AnimationTree.get_tree_root().get_node("ExtraAnimation")
+@onready var second_attack_timer: Timer = $SecondAttackTimer
+
+@onready var sword: Node3D = %Sword
+@onready var wand: Node3D = %Wand
+
+var is_attacking := false
+var squash_and_strech := 1.0:
+	set(value):
+		squash_and_strech = value
+		var negative = 1.0 + (1.0 - squash_and_strech)
+		scale = Vector3(negative, squash_and_strech, negative)
+
+const HIT_ANIMATION = "Hit_A"
+const SPELL_ANIMATION = "Spellcast_Shoot"
 
 func set_move_state(state: String) -> void:
 	move_state_machine.travel(state)
 
+
 func play_attack_animation() -> void:
 	$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	attack_state_machine.travel("Slice" if $SecondAttackTimer.time_left else "Chop")
+
+
+func play_spell_animation() -> void:
+	if not is_attacking:
+		extra_animation.animation = SPELL_ANIMATION
+		$AnimationTree.set("parameters/ExtraOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
 
 func do_defend(forward: bool) -> void:
 	var tween = create_tween()
 	tween.tween_method(_defend_change, 1.0 - float(forward), float(forward), 0.25)
 
 
+func hit() -> void:
+	extra_animation.animation = HIT_ANIMATION
+	$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
+	$AnimationTree.set("parameters/ExtraOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	is_attacking = false
+
+
 func _defend_change(value: float) -> void:
 	$AnimationTree.set("parameters/ShieldBlend/blend_amount", value)
+
+
+func _attack_toggle(value: bool):
+	is_attacking = value
--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -13,11 +13,15 @@ extends CharacterBody3D
 @export var base_speed := 4.0
 @export var run_speed := 6.0
 
-@onready var camera = $CameraController/Camera3D
+@onready var camera: Camera3D = $CameraController/Camera3D
 @onready var skin: Node3D = $GodetteSkin
+
+enum WEAPON {SWORD, WAND}
+var selected_weapon: WEAPON = WEAPON.SWORD
 
 var movement_input := Vector2.ZERO
 var is_running := false
+var speed_modifier := 1.0
 var is_defending := false:
 	set(value):
 		if not is_defending and value:
@@ -31,15 +35,40 @@ var is_defending := false:
 func _physics_process(delta: float) -> void:
 	handle_movement(delta)
 	handle_jump(delta)
-	handle_attack(delta)
+	handle_attack()
+	handle_weapon_stuff()
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		handle_hit()
+	
 	move_and_slide()
 
 
-func handle_attack(_delta: float) -> void:
+func handle_attack() -> void:
 	if Input.is_action_just_pressed("ability"):
-		skin.play_attack_animation()
+		if not skin.is_attacking:
+			match selected_weapon:
+				WEAPON.SWORD:
+					skin.play_attack_animation()
+				WEAPON.WAND:
+					skin.play_spell_animation()
+					stop_movement(0.3, 0.8)
 	
 	is_defending = Input.is_action_pressed("block")
+
+
+func handle_weapon_stuff() -> void:
+	if Input.is_action_just_pressed("switch weapon") and not skin.is_attacking:
+		match selected_weapon:
+			WEAPON.SWORD:
+				selected_weapon = WEAPON.WAND
+				skin.sword.hide()
+				skin.wand.show()
+			WEAPON.WAND:
+				selected_weapon = WEAPON.SWORD
+				skin.sword.show()
+				skin.wand.hide()
+		do_squash_and_stretch(1.1, 0.15)
 
 
 func handle_movement(delta: float) -> void:
@@ -56,8 +85,8 @@ func handle_movement(delta: float) -> void:
 	var vel_2d = Vector2(velocity.x, velocity.z)
 	
 	if movement_input != Vector2.ZERO:
-		vel_2d += (movement_input * speed * delta)
-		vel_2d = vel_2d.limit_length(speed)
+		vel_2d += (movement_input * speed * delta * 8.0)
+		vel_2d = vel_2d.limit_length(speed) * speed_modifier
 		skin.set_move_state("Running")
 		var target_angle = -movement_input.angle() + PI/2
 		skin.rotation.y = rotate_toward(skin.rotation.y, target_angle, delta * 6.0)
@@ -72,11 +101,29 @@ func handle_movement(delta: float) -> void:
 func handle_jump(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_velocity
+		do_squash_and_stretch(1.2, 0.15)
 	
 	if not is_on_floor():
 		skin.set_move_state("Jump")
 	
 	velocity.y -= (get_gravity_local() * delta)
+
+
+func handle_hit() -> void:
+	skin.hit()
+	stop_movement(0.3, 0.8)
+
+
+func do_squash_and_stretch(value: float, duration: float = 0.1) -> void:
+	var tween = create_tween()
+	tween.tween_property(skin, "squash_and_strech", value, duration)
+	tween.tween_property(skin, "squash_and_strech", 1.0, duration * 1.8).set_ease(Tween.EASE_OUT)
+
+
+func stop_movement(start_duration: float, end_duration: float) -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "speed_modifier", 0.0, start_duration)
+	tween.tween_property(self, "speed_modifier", 1.0, end_duration)
 
 
 func get_gravity_local() -> float:
